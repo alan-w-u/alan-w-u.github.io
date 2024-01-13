9ec9b71b59bb37db03ac9d487a55e2a6a248a733
alanwu04 <alanwu0004@gmail.com>
2024-01-12 18:05:07 -0800

    Initial basic website commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Constants
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Options for the IntersectionObserver
+const options = {
+    threshold: 0.75, // Threshold for amount of section visible (%) before section is highlighted in the navigation bar
+};
+
+// Highlight the section that is being viewed on the navigation bar
+const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            navLinks.forEach(navLink => {
+                navLink.classList.remove("active");
+                if (navLink.getAttribute("href").substring(1) == entry.target.id) {
+                    navLink.classList.add("active");
+                }
+            });
+        }
+    });
+}, options);
+
+// Observe to update what section is being viewed
+sections.forEach(section => {
+    observer.observe(section);
+});
