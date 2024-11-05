11f331a3bb52ff84b3d98484acf99ee351e13dee
alan-w-u <alanwu0004@gmail.com>
2024-11-04 19:44:13 -0700

    Refactor code styling and website appearance

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const navLinks = document.querySelectorAll(".nav-link");
 const projects = document.querySelectorAll("#projects figure");
 
 // Options for the IntersectionObserver
-const options = {
+const observerOptions = {
   threshold: 0.15, // Threshold for amount of section visible (%) before section is highlighted in the navigation bar
+  rootMargin: "-50px 0px 0px 0px" // Pre-empt entry into section to improve responsiveness
 };
 
 // Highlight the section that is being viewed on the navigation bar
@@ -13,29 +14,16 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       navLinks.forEach(navLink => {
-        navLink.classList.remove("active");
-        if (navLink.getAttribute("href").substring(1) == entry.target.id) {
-          navLink.classList.add("active");
+        navLink.classList.remove("selected");
+        if (navLink.getAttribute("href").substring(1) === entry.target.id) {
+          navLink.classList.add("selected");
         }
       });
     }
   });
-}, options);
+}, observerOptions);
 
 // Observe to update what section is being viewed
 sections.forEach(section => {
   observer.observe(section);
-});
-
-// Detect if using a touchscreen device
-document.addEventListener("touchstart", () => {
-  // Make project descriptions visible
-  projects.forEach(project => {
-    project.style.fontSize = "1rem";
-  });
-
-  // Make nav backgrounds transparent
-  navLinks.forEach(navLink => {
-    navLink.style.backgroundColor = "transparent";
-  });
 });
