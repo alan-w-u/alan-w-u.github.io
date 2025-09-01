a682f3fd48e143f4df1846a9e3dacaf758137fd5
alan-w-u <alanwu0004@gmail.com>
2025-09-01 01:45:10 -0700

    about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,23 @@
+import GlassCard from '../components/GlassCard'
 import styles from './About.module.css'
 
 export default function About() {
   return (
     <section id="about" className={styles['about']}>
-      
+      <div className={styles['info']}>
+        <GlassCard className={styles['picture']}>
+          <img src="/alanwu.jpg" alt="Alan Wu" draggable="false" />
+        </GlassCard>
+        <GlassCard className={styles['profile']}>
+          <p className={styles['name']}>Alan Wu</p>
+          <p className={styles['degree']}>Computer Science @ UBC</p>
+        </GlassCard>
+        <GlassCard className={styles['intro']}>
+          Hi!
+          I'm a passionate developer who thrives in collaborative environments and is always eager to explore emerging technologies.
+          Thanks for visiting and feel free to connect!
+        </GlassCard>
+      </div>
     </section>
   )
 }
