9c8e396d0998f8a9cdd465677f38895176f68677
alan-w-u <alanwu0004@gmail.com>
2025-08-31 18:01:40 -0700

    glass

---
+++ src/components/GlassCard.tsx
@@ -0,0 +1,14 @@
+import styles from './GlassCard.module.css'
+
+type GlassCardProps = {
+  children: React.ReactNode
+  className?: string
+}
+
+export default function GlassCard({ children, className }: GlassCardProps) {
+  return (
+    <div className={`${styles['glass-card']} glass ${className}`}>
+      {children}
+    </div>
+  )
+}
