b65661d7c9b70657da3e934ee6f3022b5bdca969
alan-w-u <alanwu0004@gmail.com>
2025-09-01 21:52:32 -0700

    contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,19 @@ import styles from './Contact.module.css'
 export default function Contact() {
   return (
     <section id="contact" className={styles['contact']}>
-      
+      <h1 className="title">Contact</h1>
+      <h2 className={styles['connect']}>Feel free to connect!</h2>
+      <div className={styles['cards']}>
+        <a href="mailto:alanwu0004@gmail.com" target="_blank">
+          <img src="/email.svg" alt="Email" draggable="false" />
+        </a>
+        <a href="https://www.linkedin.com/in/alan-w-u/" target="_blank">
+          <img src="/linkedin.svg" alt="LinkedIn" draggable="false" />
+        </a>
+        <a href="https://github.com/alan-w-u" target="_blank">
+          <img src="/github.svg" alt="GitHub" draggable="false" />
+        </a>
+      </div>
     </section>
   )
 }
